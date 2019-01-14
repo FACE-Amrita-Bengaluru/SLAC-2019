5a74a5308bb87716526799a9900e1649f65bcac6
Abhishek Sai <abhishekkandikattu@gmail.com>
2019-01-14 14:57:50 +0530

    Changed timer to MArch 3rd

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "December 14, 2019 18:03:26",
+	date: "March 3, 2019 00:00:00",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()

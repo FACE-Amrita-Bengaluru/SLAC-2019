b503d64f10829ac1dfbb8917935f8055b8e032b8
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2019-01-14 21:21:56 +0530

    Refactor and Firebase Init

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,22 @@
-$(window).load(function(){
-     $('.preloader').fadeOut('slow');
+$(window).load(function() {
+	$('.preloader').fadeOut('slow');
 });
-
 
 /* =Main INIT Function
 -------------------------------------------------------------- */
 function initializeSite() {
-
-	"use strict";
+	'use strict';
 
 	//OUTLINE DIMENSION AND CENTER
 	(function() {
-	    function centerInit(){
-
+		function centerInit() {
 			var sphereContent = $('.sphere'),
 				sphereHeight = sphereContent.height(),
 				parentHeight = $(window).height(),
 				topMargin = (parentHeight - sphereHeight) / 2;
 
 			sphereContent.css({
-				"margin-top" : topMargin+"px"
+				'margin-top': topMargin + 'px'
 			});
 
 			var heroContent = $('.hero'),
@@ -27,43 +24,56 @@ function initializeSite() {
 				heroTopMargin = (parentHeight - heroHeight) / 2;
 
 			heroContent.css({
-				"margin-top" : heroTopMargin+"px"
+				'margin-top': heroTopMargin + 'px'
 			});
+		}
 
-	    }
-
-	    $(document).ready(centerInit);
+		$(document).ready(centerInit);
 		$(window).resize(centerInit);
 	})();
 
-	// Init effect 
+	// Init effect
 	$('#scene').parallax();
-
-};
+}
 /* END ------------------------------------------------------- */
 
 /* =Document Ready Trigger
 -------------------------------------------------------------- */
-$(window).load(function(){
-
+$(window).load(function() {
 	initializeSite();
 	(function() {
-		setTimeout(function(){window.scrollTo(0,0);},0);
+		setTimeout(function() {
+			window.scrollTo(0, 0);
+		}, 0);
 	})();
-
 });
 /* END ------------------------------------------------------- */
 
-
 $('#countdown').countdown({
-	date: "March 3, 2019 00:00:00",
+	date: 'March 3, 2019 00:00:00',
 	render: function(data) {
-	  var el = $(this.el);
-	  el.empty()
-	    //.append("<div>" + this.leadingZeros(data.years, 4) + "<span>years</span></div>")
-	    .append("<div>" + this.leadingZeros(data.days, 2) + " <span>days</span></div>")
-	    .append("<div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div>")
-	    .append("<div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div>")
-	    .append("<div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+		var el = $(this.el);
+		el.empty()
+			//.append("<div>" + this.leadingZeros(data.years, 4) + "<span>years</span></div>")
+			.append(
+				'<div>' +
+					this.leadingZeros(data.days, 2) +
+					' <span>days</span></div>'
+			)
+			.append(
+				'<div>' +
+					this.leadingZeros(data.hours, 2) +
+					' <span>hrs</span></div>'
+			)
+			.append(
+				'<div>' +
+					this.leadingZeros(data.min, 2) +
+					' <span>min</span></div>'
+			)
+			.append(
+				'<div>' +
+					this.leadingZeros(data.sec, 2) +
+					' <span>sec</span></div>'
+			);
 	}
 });

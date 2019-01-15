a85608511dca2ade09cb061e491dcf187b8fa27c
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2019-01-15 17:37:52 +0530

    Link updates

--- public/js/main.js
+++ public/js/main.js
@@ -50,7 +50,7 @@ $(window).load(function() {
 /* END ------------------------------------------------------- */
 
 $('#countdown').countdown({
-	date: 'March 2, 2019 09:00:00',
+	date: 'February 2, 2019 09:00:00',
 	render: function(data) {
 		var el = $(this.el);
 		el.empty()

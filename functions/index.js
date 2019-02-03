108ac63f8bbb6b1eee905ff9f1b5e7255566382f
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2019-02-03 19:28:47 +0530

    Refactor, New routes, update images

--- functions/index.js
+++ functions/index.js
@@ -17,6 +17,18 @@ app.get('/Main', (req, res) => {
 	res.render('index');
 });
 
+app.get('/CodeConduct', (req, res) => {
+	res.render('codeofconduct');
+});
+
+app.get('/PrivacyPolicy', (req, res) => {
+	res.send('Will be added soon.');
+});
+
+app.get('/Terms_Conditions', (req, res) => {
+	res.send('Will be added soon.');
+});
+
 app.use((req, res, next) => {
 	res.status(404).render('404');
 });

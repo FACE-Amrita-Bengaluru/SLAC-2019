316c69706385fbf0e8c66626c6dcd8b9f60fb3bf
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2019-03-09 18:12:03 +0530

    Countdown

--- public/Main/js/main.js
+++ public/Main/js/main.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
 	$('.countdown').countdown('2019/03/30', function(event) {
 		$(this).html(
 			event.strftime(
-				'<div>%w <span>Weeks</span></div>  <div>%D <span>Days</span></div>  <div>%H<span>Hours</span></div> <div>%M<span>Minutes</span></div> <div>%S<span>Seconds</span></div>'
+				'<div>%D <span>Days</span></div>  <div>%H<span>Hours</span></div> <div>%M<span>Minutes</span></div> <div>%S<span>Seconds</span></div>'
 			)
 		);
 	});
